8209f67a1ad217b8516421f34565a9488ffa77dd
Ahmed Adekunle <medijay99@yahoo.com>
2024-08-23 22:36:38 +0100

    recent commit

--- src/app.js
+++ src/app.js
@@ -2,46 +2,48 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 const forecast = require('./utils/forecast.js');
-const geocode = require('./utils/geocode.js')
+const geocode = require('./utils/geocode.js');
 
-//set up express app
+// Set up express app
 const app = express();
 const port = process.env.PORT || 3000;
 
-//static path setting
+// Static path setting
 const dirPath = path.join(__dirname, '../public');
 app.use(express.static(dirPath));
 
-//view path setting
-app.set('views', path.join(__dirname, '../templates/partials'));
+// Views path setting
+app.set('views', path.join(__dirname, '../templates/views'));
 
-//set up template engine
+// Partials path setting
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
+
+// Set up template engine
 app.set('view engine', 'hbs');
 
-app.get('', (req,res) => {
+app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name:'MJ',
-
+        name: 'MJ'
     })
-});
+})
 
-app.get('/help', (req,res) => {
+app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
         name: 'MJ',
-        message: 'Hope this works at the time you are using it'
+        message: 'Hope this works at the time you\'re using it :P'
     })
 });
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
         name: 'MJ'
     })
 })
 
-app.get('/weather', (req,res) => {
+app.get('/weather', (req, res) => {
     const location = req.query.address;
     const jsonData = {
         title: 'Weather',
@@ -49,34 +51,41 @@ app.get('/weather', (req,res) => {
         location
     };
 
-    if (!location) {
+    // res.send(jsonData)
+
+    if(!location) {
         jsonData.location = 'No address is entered.'
+        // res.render('weather', jsonData);
         res.send(jsonData)
     }
     else {
-        geocode(location, (error, {latitude, longitude, location} = {}) => {
+        geocode(location, (error, { latitude, longitude, location } = {}) => {
             jsonData.location = location;
             if (error) {
                 jsonData.forecast = error;
                 res.send(jsonData)
+                // res.render('weather', jsonData);
             }
             else {
-                forecast({latitude, longitude }, (error, { daily, currently}) => {
+                forecast({ latitude, longitude }, (error, { daily, currently }) => {
                     if (error) {
                         jsonData.forecast = error;
                         res.send(jsonData)
+                        // res.render('weather', jsonData);
                     }
                     else {
-                        jsonData.forecast = `${daily.summary} It is currently ${currently.temperature} degrees out. There is ${currently.precipProbabilities}% chance of rain.`;
+                        jsonData.forecast = `${daily.summary} It is currently ${currently.temperature} degrees out. There is ${currently.precipProbability}% chance of rain.`;
+                        // res.render('weather', jsonData);
                         res.send(jsonData)
                     }
                 })
             }
         })
     }
+    
 })
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.render('404', {
         message: 'Something went wrong with the page you are looking for',
         title: '404',
@@ -84,10 +93,10 @@ app.get('*', (req,res) => {
     });
 })
 
-app.get('/help/*', (req,res) => {
-    res.render('404', {message: 'Help articles not found'});
+app.get('/help/*', (req, res) => {
+    res.render('404', { message: 'Help articles not found' });
 })
 
 app.listen(port, () => {
-    console.log('Server is up and running on port' + port);
+    console.log('Server is up and running on port ' + port);
 })
